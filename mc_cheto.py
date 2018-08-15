ee6f13a07f47e6b20619cea50dad5a9f5ed9cd75
ine16 <42250665+ine16@users.noreply.github.com>
2018-08-14 21:19:01 -0300

    Add files via upload

---
+++ mc_cheto.py
@@ -0,0 +1,65 @@
+#MONTECHARLY
+import numpy as np
+import random
+#from matplotlib import pyplot as plt
+#defino numero de sitios N, energias de estados a y b y longitudes asociadas
+N=10
+E_a=-1
+E_b=1
+L_a=1
+L_b=2
+#plt.rcParams['agg.path.chunksize'] = 20000
+#kk=np.zeros(1000)
+E_prom=np.zeros(1000)
+f=open('archivito.txt','w')
+print >>f,'kT','Energias'
+#para obtener el grafico de muchos valores de E vs kT
+for j in range(1000):
+	kT=0.01+j/400.
+	config=np.ones(N)
+	#x es el numero de iteraciones para que termalice (x=100*N pasos y cada paso es hacer el primer 'for' N veces)
+	x=10000
+	E_0=0
+	num_a=0
+	for i in range(x):
+		#toma la posicion j de config elegida al azar y calcula la variacion de energia deltaE
+		j=random.randint(0,(N-1))
+		deltaE=(-2)*config[j]
+		#si deltaE disminuye se queda con la modificacion en la posicion j
+		if deltaE < 0:
+			config[j]=-(config[j])
+		if deltaE > 0:
+			#este coso es la condicion para decidir si aceptar o no la modificacion de j
+			coso=random.random()
+			if coso < (np.exp(-deltaE/kT)):
+				config[j]=-(config[j])
+	for n in range(N):
+		#mide primer valor de energia (E_0)
+		E_0=E_0+config[n]
+		if config[n]==(-1):
+			num_a=num_a+1
+	energia=np.zeros(x+1)
+	energia[0]=E_0
+	#tomamos x medidas para promediar
+	for i in range(1,x+1):
+		#toma la posicion j de config elegida al azar y calcula la variacion de energia deltaE
+		j=random.randint(0,(N-1))
+		deltaE=(-2)*config[j]
+		#si deltaE disminuye se queda con la modificacion en la posicion j
+		if deltaE < 0:
+			config[j]=-(config[j])
+			num_a=num_a+1
+		if deltaE > 0:
+			#este coso es la condicion para decidir si aceptar o no la modificacion de j
+			coso=random.random()
+			if coso < (np.exp(-deltaE/kT)):
+				config[j]=-(config[j])
+				num_a=num_a-1
+		energia[i]=E_a*num_a+E_b*(N-num_a)
+	E_prom=np.mean(energia)
+	#guarda en el archivito los valores de kT y energia para graficar
+	print >>f,kT,E_prom
+	#kk[j]=kT
+f.close()
+#plt.scatter(kk,E_prom)
+#plt.show()
