4d4138759447f833dba46fce0c6f5473da5d4ded
santinoacco <42284724+santinoacco@users.noreply.github.com>
2018-08-16 17:43:54 -0300

    Update Metropolis_Algorithm.py

--- Metropolis_Algorithm.py
+++ Metropolis_Algorithm.py
@@ -21,12 +21,12 @@ a=1
 b=2
 e1=-1
 e2=1
-N=200 #cantidad de sitios
-mcs=2*N #cantidad de pasos MonteCarlo
-k=C.Boltzmann #const de Boltzmann en J/K
+N=100 #cantidad de sitios
+mcs=10*N #cantidad de pasos MonteCarlo
+#k=C.Boltzmann #const de Boltzmann en J/K
 #k2=C.Stefan_Boltzmann #const de Boltzmann en eV/K
-T=300 #temperatura en grados Kelvin
-beta = 1./k*T
+#T=300 #temperatura en grados Kelvin
+
 Mean_E=0
 Mean_L=0
 mean_E2=0
@@ -42,7 +42,8 @@ Ea2ux=0
 '''creamos la bandita/estado'''
 state = np.ones(N)
 print(state)
-def contar(state):
+
+def contar(state): #devuelve la cantidad de sitios en el estado de energía e1
     x=0
     for place in state:
         if place == e1:
@@ -50,57 +51,61 @@ def contar(state):
     return x
 
 def energy(x):
-    E=(e1-e2)*x + N*e2
+    E=e1*x + (N-x)*e2
     return E
-'''
-def p(beta,x):
-    return np.exp(-beta*(e1*x + (N-x)*e2))
-'''
-#==============================================================================
-# def length_1(x):
-#     return (b*N+(a-b)*x)
-#==============================================================================
 
 
-contar(state)
+def dist(kT,Delta):
+    return np.exp(-Delta/kT)
 
-print(energy(contar(state)))
-
-def dist(Delta): return np.exp(-beta*Delta)
 
 y=[]
 x=[]
+
+
+
+
 for b in range(0,50): #variamos la temperatura
-    T = 10 + b*10
-    x.append(T)
-    print( 'va por %s'% T)
+    kT = 0.01 + b
+    x.append(kT)
+    print( 'va por %s'% kT)
+    #termalizar
     for i in range(mcs):
-        
         for j in range(N):
             
-            q=int(rnd.uniform(0,N)) #nro random para elegir posicion de la cadena
+            q=rnd.randint(0,N) #nro random para elegir posicion de la cadena
             E=energy(contar(state))
             state[q]= -state[q]
             Eq=energy(contar(state))
-            Delta = E-Eq
-#==============================================================================
-#             if Delta<0:
-#                 break
-#==============================================================================
+            Delta = (-2)*state[q]
     
             if Delta>0:
                 p=rnd.uniform(0,1)
-                if p<dist(Delta):
+                if p<dist(kT,Delta):
                     state[q]= -state[q]
-                    
+    
+    #cambio los estados y mido       
+    
+    for i in range(mcs):
+        q=rnd.randint(0,N) #nro random para elegir posicion de la cadena
+        E=energy(contar(state))
+        state[q]= -state[q]
+        Eq=energy(contar(state))
+        Delta = (-2)*state[q]
+        if Delta>0:
+             p=rnd.uniform(0,1)
+             if p<dist(kT,Delta):
+                 state[q]= -(state[q])
+                 #Eq=energy(contar(state))
         Eaux+=energy(contar(state))
+        
        # Laux+=length_1(contar(state))
         
     Mean_E= Eaux/mcs
     y.append(Mean_E)
 #Mean_L           
 #    print(state)
-      
+
 
 '''graficamos'''
   
