e5b008e2f0cc103ed3c3fe0979bc9ac9b1bde7fa
ine16 <42250665+ine16@users.noreply.github.com>
2018-08-18 21:51:42 -0300

    Update mc_cheto.py

--- mc_cheto.py
+++ mc_cheto.py
@@ -1,6 +1,7 @@
 #MONTECHARLY
 import numpy as np
 import random
+import matplotlib.pyplot as plt	
 #from matplotlib import pyplot as plt
 #defino numero de sitios N, energias de estados a y b y longitudes asociadas
 N=10
@@ -8,14 +9,16 @@ E_a=-1
 E_b=1
 L_a=1
 L_b=2
+E_prom=0
+
 #plt.rcParams['agg.path.chunksize'] = 20000
 #kk=np.zeros(1000)
-E_prom=np.zeros(1000)
+#E_prom=np.zeros(1000)
 f=open('archivito.txt','w')
-print >>f,'kT','Energias'
+print('kT','Energias',file=f)
 #para obtener el grafico de muchos valores de E vs kT
-for j in range(1000):
-	kT=0.01+j/400.
+for num in range(1000):
+	kT=0.01+num/400.
 	config=np.ones(N)
 	#x es el numero de iteraciones para que termalice (x=100*N pasos y cada paso es hacer el primer 'for' N veces)
 	x=10000
@@ -58,8 +61,15 @@ for j in range(1000):
 		energia[i]=E_a*num_a+E_b*(N-num_a)
 	E_prom=np.mean(energia)
 	#guarda en el archivito los valores de kT y energia para graficar
-	print >>f,kT,E_prom
+	print(kT,E_prom,file=f)
 	#kk[j]=kT
 f.close()
+X, Y = [], []
+for line in open(nombreArchivo, 'r'):
+	values = [float(s) for s in line.split()]
+	X.append(values[0])
+	Y.append(values[1])
+plt.scatter(X, Y)
+plt.show()
 #plt.scatter(kk,E_prom)
 #plt.show()
